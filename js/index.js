a8bf09eb9ce69fff5c8435587aba406b1cd9a8ce
Farhad Siraj <farhadsiraj@gmail.com>
2021-05-24 18:40:22 -0400

    added a toTheTop button

--- js/index.js
+++ js/index.js
@@ -1,6 +1,10 @@
 const navToggle = document.querySelector('.nav-toggle');
 const navLinks = document.querySelectorAll('.nav__link');
 
+const target = document.querySelector('header');
+var scrollToTopBtn = document.querySelector('.scrollToTopBtn');
+const rootElement = document.documentElement;
+
 navToggle.addEventListener('click', () => {
   document.body.classList.toggle('nav-open');
 });
@@ -10,3 +14,27 @@ navLinks.forEach((link) => {
     document.body.classList.remove('nav-open');
   });
 });
+
+function scrollToTop() {
+  rootElement.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  });
+}
+
+function callback(entries, observer) {
+  // The callback will return an array of entries, even if you are only observing a single item
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      // Show button
+      scrollToTopBtn.classList.remove('showBtn');
+    } else {
+      // Hide button
+      scrollToTopBtn.classList.add('showBtn');
+    }
+  });
+}
+
+scrollToTopBtn.addEventListener('click', scrollToTop);
+let observer = new IntersectionObserver(callback);
+observer.observe(target);
